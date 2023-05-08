60811502a149b71ca43d00db8d98e6905fad847d
TioIvan <132549783+TioIvan@users.noreply.github.com>
2023-05-08 11:14:01 +0200

    implemented def

--- chat.py
+++ chat.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import openai
 
-
 #-----------------------------------KEYS AND LOCATIONS-------------------------------------------------
 
 
@@ -68,106 +67,109 @@ def position(df,image):
 
     return df
 
-position(df,image)
 #---------        --------      ---------DESCRIPTION---------        ---------         ------------
 
-print(df)
-
-CARS = []
-TL = []
-TS = []
-PERSON = []
-BICYCLES = []
-OTHERS = []
-
-for a in range(0,len(df.index)):
-
-    if df.loc[a,'%s'%(class_name)] == 'car':
+def ChatGPT(df):
         
-        if df.loc[a,'%s'%(state_name)] == 'front':
-            CARS.append('A car approaching from %s'%(df.loc[a,'%s'%(pos_name)]))
+    CARS = []
+    TL = []
+    TS = []
+    PERSON = []
+    BICYCLES = []
+    OTHERS = []
+    
+    for a in range(0,len(df.index)):
+    
+        if df.loc[a,'%s'%(class_name)] == 'car':
             
-        elif df.loc[a,'%s'%(state_name)] == 'rear':
-            CARS.append('A car %s'%(df.loc[a,'%s'%(pos_name)]))
+            if df.loc[a,'%s'%(state_name)] == 'front':
+                CARS.append('A car approaching from %s'%(df.loc[a,'%s'%(pos_name)]))
+                
+            elif df.loc[a,'%s'%(state_name)] == 'rear':
+                CARS.append('A car %s'%(df.loc[a,'%s'%(pos_name)]))
+                
+            else:
+                CARS.append('A car %s'%(df.loc[a,'%s'%(pos_name)])) #SIDE OF THE CAR
+                
+        
+        elif df.loc[a,'%s'%(class_name)] == 'traffic light':
+            # CARS.append(df.loc[a,'0'])
+            TL.append('A %s %s' %(df.loc[a,'%s'%(state_name)], df.loc[a,'%s'%(class_name)]))
+       
+        
+        elif df.loc[a,'%s'%(class_name)] == 'traffic sign':
+            # CARS.append(df.loc[a,'0'])
+            TS.append('A "%s" traffic sign'%(df.loc[a,'%s'%(state_name)]))
             
+        elif df.loc[a,'%s'%(class_name)] == 'person':
+            # CARS.append(df.loc[a,'0'])
+            PERSON.append(df.loc[a,'%s'%(class_name)])
+            PERSON.append(df.loc[a,'%s'%(state_name)])
+            
+        elif df.loc[a,'%s'%(class_name)] == 'bicycle':
+            
+            if df.loc[a,'%s'%(state_name)] == 'front':
+                BICYCLES.append('A bicycle approaching from %s'%(df.loc[a,'%s'%(pos_name)]))
+                
+            elif df.loc[a,'%s'%(state_name)] == 'rear':
+                BICYCLES.append('A bicycle %s'%(df.loc[a,'%s'%(pos_name)]))
+                
+            else:
+                BICYCLES.append('A bicycle %s'%(df.loc[a,'%s'%(pos_name)])) #SIDE OF THE CAR
+                 
         else:
-            CARS.append('A car %s'%(df.loc[a,'%s'%(pos_name)])) #SIDE OF THE CAR
+            # CARS.append(df.loc[a,'0'])
+            OTHERS.append(df.loc[a,'%s'%(class_name)])
+            OTHERS.append(df.loc[a,'%s'%(state_name)])
             
-    
-    elif df.loc[a,'%s'%(class_name)] == 'traffic light':
-        # CARS.append(df.loc[a,'0'])
-        TL.append('A %s %s' %(df.loc[a,'%s'%(state_name)], df.loc[a,'%s'%(class_name)]))
-   
-    
-    elif df.loc[a,'%s'%(class_name)] == 'traffic sign':
-        # CARS.append(df.loc[a,'0'])
-        TS.append('A "%s" traffic sign'%(df.loc[a,'%s'%(state_name)]))
-        
-    elif df.loc[a,'%s'%(class_name)] == 'person':
-        # CARS.append(df.loc[a,'0'])
-        PERSON.append(df.loc[a,'%s'%(class_name)])
-        PERSON.append(df.loc[a,'%s'%(state_name)])
-        
-    elif df.loc[a,'%s'%(class_name)] == 'bicycle':
-        
-        if df.loc[a,'%s'%(state_name)] == 'front':
-            BICYCLES.append('A bicycle approaching from %s'%(df.loc[a,'%s'%(pos_name)]))
             
-        elif df.loc[a,'%s'%(state_name)] == 'rear':
-            BICYCLES.append('A bicycle %s'%(df.loc[a,'%s'%(pos_name)]))
-            
-        else:
-            BICYCLES.append('A bicycle %s'%(df.loc[a,'%s'%(pos_name)])) #SIDE OF THE CAR
-             
-    else:
-        # CARS.append(df.loc[a,'0'])
-        OTHERS.append(df.loc[a,'%s'%(class_name)])
-        OTHERS.append(df.loc[a,'%s'%(state_name)])
+    #IF empty
+    if bool(CARS) == False:
+        CARS.append('There are no cars in sight')
         
+    if bool(TL) == False:
+        TL.append('There are no traffic lights in sight')
         
-#IF empty
-if bool(CARS) == False:
-    CARS.append('There are no cars in sight')
+    if bool(TS) == False:
+        TS.append('There are no traffic signs in sight')
+        
+    if bool(PERSON) == False:
+        PERSON.append('There are no pedestrians in sight')
+        
+    if bool(BICYCLES) == False:
+        BICYCLES.append('There are no bicycles in sight')
     
-if bool(TL) == False:
-    TL.append('There are no traffic lights in sight')
+    if bool(OTHERS) == False:
+        OTHERS.append('there are no more objects than the ones mentioned above')
     
-if bool(TS) == False:
-    TS.append('There are no traffic signs in sight')
     
-if bool(PERSON) == False:
-    PERSON.append('There are no pedestrians in sight')
     
-if bool(BICYCLES) == False:
-    BICYCLES.append('There are no bicycles in sight')
+    #--------------------------------------ChatGPT-------------------------------------------------
+    
+    
+    # Set up the OpenAI API client
+    openai.api_key = openAI_key
+    
+    # Set up the model and prompt
+    model_engine = "text-davinci-003"
+    
+    prompt = "Assume you are driving in %s. You are driving in a %s area at %d km/h. You see the following cars: %s. You see the following traffic signs: %s. You see the following traffic lights: %s. You see the following pedestrians: %s. You see the following bicyclist: %s. Additionally, you see: %s. Generate a multiple choice question with the following answer choices: 'Let go of the gas pedal', 'Brake' or 'Do nothing'. After showing the question and answers, pick your answer. Give your thorough reason behind it."%(country, location, speed, CARS, TS, TL, PERSON, BICYCLES, OTHERS)
+    print(prompt)
+    
+    
+    # Generate a response ChatGPT
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    
+    response = completion.choices[0].text
+    
+    return(prompt,response)
 
-if bool(OTHERS) == False:
-    OTHERS.append('there are no more objects than the ones mentioned above')
-
-
-
-#--------------------------------------ChatGPT-------------------------------------------------
-
-
-# Set up the OpenAI API client
-openai.api_key = openAI_key
-
-# Set up the model and prompt
-model_engine = "text-davinci-003"
-
-prompt = "Assume you are driving in %s. You are driving in a %s area at %d km/h. You see the following cars: %s. You see the following traffic signs: %s. You see the following traffic lights: %s. You see the following pedestrians: %s. You see the following bicyclist: %s. Additionally, you see: %s. Generate a multiple choice question with the following answer choices: 'Let go of the gas pedal', 'Brake' or 'Do nothing'. After showing the question and answers, pick your answer. Give your thorough reason behind it."%(country, location, speed, CARS, TS, TL, PERSON, BICYCLES, OTHERS)
-print(prompt)
-
-
-# Generate a response ChatGPT
-completion = openai.Completion.create(
-    engine=model_engine,
-    prompt=prompt,
-    max_tokens=1024,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
-
-response = completion.choices[0].text
-print(response)
+df = position(df,image)
+ChatGPT(df)
